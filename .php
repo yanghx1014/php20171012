de1745f32608248e5bf5ed80794b4277fab35c69
DMer <710072999@gmail.com>
2017-11-07 18:18:45 +0800

    留言板_51

--- .php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2017/11/7
- * Time: 13:14
- */
--- message.php
+++ message.php
@@ -9,17 +9,46 @@
     <link rel="stylesheet" href="./bootstrap/js/jquery-3.2.1.js">
     <link rel="stylesheet" href="./bootstrap/js/bootstrap.js">
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+    <style>
+        body {
+            padding: 30px;
+        }
+    </style>
 </head>
 <body>
+<?php
+    $link = mysqli_connect("localhost","root","root","php20171107");
+?>
     <div class="container">
         <div class="row">
-            <form action="" class="form-horizontal">
+            <form action="" method="post" class="form-horizontal">
                 <div class="form-group">
-                    <label for="" class="form-control">主题：</label>
-                    <input type="text" class="form-control" id="title">
+                    <label for="" class="control-label col-md-2">主题：</label>
+                    <div class="col-md-10">
+                        <input type="text" class="form-control" name="title" id="title">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-offset-2 col-md-10">
+                        <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-offset-2 col-md-10">
+                        <input type="text" value="发布留言" class="btn btn-primary">
+                    </div>
                 </div>
             </form>
         </div>
     </div>
+<?php
+    if ($_POST['title']){
+        $title = $_POST['title'];
+        $content = $_POST['content'];
+
+        $sql = "INSERT INTO message(title,content) VALUE ($title,$content)";
+        $res = $mysqli_query($sql,$link);
+    }
+?>
 </body>
 </html>
