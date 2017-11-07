7f613d2b4c221e6f46459cc0ea3a8d5cf737b402
DMer <710072999@gmail.com>
2017-11-07 13:46:59 +0800

    留言板_51

---
+++ message.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>留言板</title>
+    <link rel="stylesheet" href="./bootstrap/js/jquery-3.2.1.js">
+    <link rel="stylesheet" href="./bootstrap/js/bootstrap.js">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <form action="" class="form-horizontal">
+                <div class="form-group">
+                    <label for="" class="form-control">主题：</label>
+                    <input type="text" class="form-control" id="title">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
